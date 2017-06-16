1a3cd9ae359973024104bf0071abd932f2218a69
zhangzhi0903 <18310127735@163.com>
2017-06-16 15:25:28 +0800

    Initial Commit

---
+++ ZGProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGProtocol
+//
+//  Created by offcn_zcz32036 on 2017/6/16.
+//  Copyright © 2017年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZGProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGProtocol
+//
+//  Created by offcn_zcz32036 on 2017/6/16.
+//  Copyright © 2017年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
